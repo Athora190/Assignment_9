afecb3f3032af2753edba2f8167f2e58fbb6104d
Athora190 <jademarte7@gmail.com>
2021-01-15 15:38:42 -0500

    first name last name

--- src/pages/SignUpValidation.js
+++ src/pages/SignUpValidation.js
@@ -1,9 +1,19 @@
-import React from 'react';
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
+
+
+function validateFirstLastName(value){
+    if(value.length < 2 )
+    {
+        return "invalid";
     }
+    if(value.length > 20)
+    {
+        return "that's a long name";
+    }
+    if(/\d/.test(value))
+    {
+        return "Does your name really include numbers?";
+    }
+    return null;
+
 }
- 
-export default ;
+export {validateFirstLastName};
